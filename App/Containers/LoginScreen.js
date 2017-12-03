6ba618baf071bb59d8de0ea2541b25b7f63e21ba
anhltse03448 <linhhoang830@gmail.com>
2017-12-03 11:12:10 +0700

    Add location

--- App/Containers/LoginScreen.js
+++ App/Containers/LoginScreen.js
@@ -52,7 +52,7 @@ class LoginScreen extends Component {
   }
 
   signInWithFacebook = () => {
-    LoginManager.logInWithReadPermissions(['public_profile', 'email']).then(
+    LoginManager.logInWithReadPermissions(['public_profile', 'email', 'user_location']).then(
       function (result) {
         if (result.isCancelled) {
         } else {
